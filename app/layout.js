4d594f61506e006637638a592298aea404f4484d
SanjaM <sanjamilosdev@gmail.com>
2024-04-25 00:54:41 +0800

    'fix-five'

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Yuya Portfolio',
+  title: 'Hannah Portfolio',
   description: 'Created by RTR',
 }
 
